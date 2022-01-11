983c732a16fc7d4307bd6cb6de61fb9e81b9482a
David Stevens <teamjtf4@gmail.com>
2022-01-11 17:08:03 -0500

    Added IR support, HTTP, and TCP modes

--- m5-atom-ugreen-kvm-Changer/src/main.cpp
+++ m5-atom-ugreen-kvm-Changer/src/main.cpp
@@ -17,6 +17,19 @@ bool portalRunning = false;
 const char * netName = "M5-KVM";
 void connectToNetwork();
 
+// HTTP Webpage Setup
+WiFiServer server(80);
+void WiFiEvent(WiFiEvent_t event);
+void printPage(WiFiClient client);
+String request = "";
+
+// TCP Server Setup
+WiFiServer serverTCP(23);
+WiFiClient RemoteClient;
+String CheckMessage(String message);
+void checkForConnections();
+void receiveData();
+
 // Logger Setup
 void logger(String strLog, String strType);
 
@@ -43,6 +56,7 @@ uint16_t nextData[71] = {9066, 4504,  584, 550,  584, 552,  582, 552,  582, 552,
 //IR Setup
 uint16_t kIRLed = 12;
 IRsend irsend(kIRLed);
+int currentInput = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -51,18 +65,6 @@ void setup() {
 
   //Initialize IR
   Serial.begin(115200, SERIAL_8N1);
-
-  preferences.begin("M5-KVM", false);
-
-  // added to clear out corrupt prefs
-  //preferences.clear();
-  logger("Reading preferences", "info-quiet");
-  if(preferences.getString("DevMode").length() > 0) {
-    String new_deviceMode = preferences.getString("DevMode");
-    new_deviceMode.toCharArray(deviceMode, 20);
-  }
- 
-  preferences.end();
 
   // Initialize the M5StickC object
   logger("Initializing M5StickC object.", "info-quiet");
@@ -78,11 +80,39 @@ void setup() {
     delay(200);
   }
 
+  logger("IP Address:", "info");
+  logger(String(WiFi.localIP()), "info");
+  logger("\n", "info");
+  logger("Mac Address:", "info");
+  logger(String(WiFi.macAddress()), "info");
+
+  preferences.begin("M5-KVM", false);
+  String new_deviceMode = preferences.getString("DeviceMode");
+  new_deviceMode.toCharArray(deviceMode, 20);
+  logger("Device Mode:" + new_deviceMode, "info");
+  // added to clear out corrupt prefs
+  //preferences.clear();
+  preferences.end();
+
+  if (String(deviceMode) == "HTTP") {
+    server.begin();
+    logger("HTTP server started", "info");
+  } else if (String(deviceMode) == "TCP") {
+    serverTCP.begin();
+    logger("TCP server started", "info");
+  } else {
+    server.begin();
+    logger("Mode Discovery Error", "info");
+    logger("HTTP server started", "info");
+  }
+  
+  
+
 }
 
 void loop() {
 
-  if (millis() >= 86400000) {
+  if (millis() >= 21600000) {
     rebootNow();
   }
 
@@ -97,6 +127,77 @@ void loop() {
     wm.resetSettings();
     ESP.restart();
   }
+
+  if (btnM5.isClick()) {
+    irsend.sendRaw(nextData, 67, 38);
+      if (currentInput == 1) {
+        currentInput = 2;
+        logger("Input 2", "debug");
+      } else if (currentInput == 2) {
+        currentInput = 3;
+        logger("Input 3", "debug");
+      } else if (currentInput == 3) {
+        currentInput = 1;
+        logger("Input 2", "debug");
+      }
+  }
+
+  if (String(deviceMode) == "HTTP") {
+    // Check if Client Connected
+  WiFiClient client = server.available();
+  if (!client) {return;}
+
+  // Read HTTP Request
+  request = client.readStringUntil('\r');
+
+  if ( request.indexOf("INPUT_1") > 0 )  { 
+      irsend.sendRaw(input1Data, 67, 38); // Send IR Data
+      delay(1000);
+      irsend.sendRaw(input1Data, 67, 38); // Send IR Data
+      logger("Input 1", "debug");
+      printPage(client);
+      currentInput = 1;     
+    } else if  ( request.indexOf("INPUT_2") > 0 ) { 
+      irsend.sendRaw(input2Data, 67, 38);
+      delay(1000);
+      irsend.sendRaw(input2Data, 67, 38);
+      logger("Input 2", "debug");
+      printPage(client);
+      currentInput = 2;   
+    } else if  ( request.indexOf("INPUT_3") > 0 ) { 
+      irsend.sendRaw(input3Data, 67, 38);
+      delay(1000);
+      irsend.sendRaw(input3Data, 67, 38);
+      logger("Input 3", "debug");
+      printPage(client);
+      currentInput = 3;   
+    } else if  ( request.indexOf("NEXT") > 0 ) { 
+      irsend.sendRaw(nextData, 67, 38);
+      if (currentInput == 1) {
+        currentInput = 2;
+        logger("Input 2", "debug");
+      } else if (currentInput == 2) {
+        currentInput = 3;
+        logger("Input 3", "debug");
+      } else if (currentInput == 3) {
+        currentInput = 1;
+        logger("Input 2", "debug");
+      }
+      printPage(client);
+    }
+    else if (!client) 
+    {
+      client.flush();
+    } else 
+    {
+      printPage(client);
+    }
+  } else if (String(deviceMode) == "TCP") {
+    checkForConnections();
+    receiveData();
+  }
+
+  
 }
 
 void WiFiEvent(WiFiEvent_t event) {
@@ -133,8 +234,8 @@ void saveParamCallback() {
   //saveEEPROM();
   logger("Saving new settings.", "info-quiet");
   logger(str_devMode, "info-quiet");
-  preferences.begin("tally-arbiter", false);
-  preferences.putString("devMode", str_devMode);
+  preferences.begin("M5-KVM", false);
+  preferences.putString("DeviceMode", str_devMode);
   preferences.end();
 
 }
@@ -215,3 +316,97 @@ void logger(String strLog, String strType) {
     Serial.println(strLog);
   }
 }
+
+// Webpage Information
+String deviceIP = String(WiFi.localIP());
+
+String header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+String html_1 = "<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1.0'/><meta charset='utf-8'><style>body {font-size:140%;} #main {display: table; margin: auto;  padding: 0 10px 0 10px; } h2,{text-align:center; } .button { padding:10px 10px 10px 10px; width:100%;  background-color: #4CAF50; font-size: 120%;}</style><title>TV Control</title></head><body><div id='main'><h2>TV Control</h2>";
+String html_2 = "<form id='F1' action='INPUT_1'><input class='button' onclick='" + deviceIP + ";' type='submit' value='Input 1' ></form><br>";
+String html_3 = "<form id='F1' action='INPUT_2'><input class='button' type='submit' value='Input 2' ></form><br>";
+String html_4 = "<form id='F1' action='INPUT_3'><input class='button' type='submit' value='Input 3' ></form><br>";
+String html_5 = "<form id='F1' action='NEXT'><input class='button' type='submit' value='Next Input' ></form><br>";
+String html_6 = "</div></body></html>";
+String input_1 = "<t>The TV Is currently using Input 1</t>";
+String input_2 = "<t>The TV Is currently using Input 2</t>";
+String input_3 = "<t>The TV Is currently using Input 3</t>";
+
+void printPage(WiFiClient client) {
+  client.print(header);
+  client.print(html_1);
+  client.print(html_2);
+  client.print(html_3);
+  client.print(html_4);
+  client.print(html_5);
+  client.print(html_6);
+  if (currentInput == 1) {
+    client.print(input_1);
+  } else if (currentInput == 2) {
+    client.print(input_2);
+  } else if (currentInput == 3) {
+    client.print(input_3);
+  }
+  delay(500);
+}
+
+void checkForConnections() {
+  if (serverTCP.hasClient()) {
+    // If we are already connected to another computer, 
+    // then reject the new connection. Otherwise accept
+    // the connection. 
+    if (RemoteClient.connected()) {
+      Serial.println("Connection rejected");
+      serverTCP.available().stop();
+    }
+    else {
+      Serial.println("Connection accepted");
+      RemoteClient = serverTCP.available();
+    }
+  }
+}
+
+void receiveData() {
+  while (RemoteClient.connected() && RemoteClient.available()) {
+    String message = RemoteClient.readStringUntil('\n');
+    message.trim();
+    String response = CheckMessage(message);
+    Serial.println(response);
+    char responseCharArray[response.length() + 1];
+    strcpy(responseCharArray, response.c_str());
+    RemoteClient.write(responseCharArray);
+  }
+}
+
+String CheckMessage(String message) {
+  String response = "Invalid command.\n";
+  
+  Serial.println("Checking command: " + message);
+  if (message == "input1") {
+    logger("Input 1", "info");
+    irsend.sendRaw(input1Data, 67, 38); // Send IR Data
+    delay(1000);
+    irsend.sendRaw(input1Data, 67, 38); // Send IR Data
+    response = "Input 1\n";
+  } else if (message == "input2") {
+    logger("Input 2", "info");
+    irsend.sendRaw(input2Data, 67, 38); // Send IR Data
+    delay(1000);
+    irsend.sendRaw(input2Data, 67, 38); // Send IR Data
+    response = "Input 2\n";
+  } else if (message == "input3") {
+    logger("Input 3", "info");
+    irsend.sendRaw(input3Data, 67, 38); // Send IR Data
+    delay(1000);
+    irsend.sendRaw(input3Data, 67, 38); // Send IR Data
+    response = "Input 3\n";
+  } else if (message == "next") {
+    logger("Next", "info");
+    irsend.sendRaw(nextData, 67, 38); // Send IR Data
+    delay(1000);
+    irsend.sendRaw(nextData, 67, 38); // Send IR Data
+    response = "Next Input\n";
+  }
+
+  return response;
+}
+
